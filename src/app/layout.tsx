5c444d25f620a3266f1f394858810d9890b5eb54
afjal <123afjal2005@sjcem.edu.in>
2026-06-18 13:34:30 +0530

    Update favicon and logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,6 +32,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="scroll-smooth">
+   
       <body className="min-h-full antialiased">{children}</body>
     </html>
   );
